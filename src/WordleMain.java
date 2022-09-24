edf83b728898e6af6645fc8a66e37fc42d4b274e
sbottingota <saku.bottingota@gmail.com>
2022-09-24 07:26:40 +0100

    Committing guessableWords, which has all the guessable five-letter words in it, as well as code in WordleMain.java to retrieve those words.

--- src/WordleMain.java
+++ src/WordleMain.java
@@ -1,12 +1,19 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.*;
 
 public class WordleMain {
     private static final String[] guessableWords = {"crane"};
 
     public static void main(String[] args) {
-
     }
+
+    private static String[] getUsableWords() throws IOException {
+        String fileContents = Files.readString(Paths.get("usableWords/guessableWords"));
+        return fileContents.split(" ");
+    }
+
     public static String getWordleWord() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a five letter word:");
