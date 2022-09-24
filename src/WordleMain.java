423fc9afd2a75483afebf1f43a90013251b96cee
sbottingota <saku.bottingota@gmail.com>
2022-09-24 19:04:02 +0100

    Added the colored keyboard to WordleMain.java, as well as a way to change the colors of the keys, and to print it to System.out.

--- src/WordleMain.java
+++ src/WordleMain.java
@@ -2,9 +2,19 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
 
-public class WordleMain {
+public final class WordleMain {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {}
+
+    //returns a random word from possibleWords
+    public static String getWordle() throws IOException {
+        String fileContents = Files.readString(Paths.get("./usableWords/possibleWordles"));
+        String[] wordlesArray = fileContents.split(" ");
+
+        Random random = new Random();
+        String wordle = wordlesArray[random.nextInt(wordlesArray.length)];
+
+        return wordle;
     }
 
     //reads and returns guessableWords as an array
@@ -31,14 +41,50 @@ public class WordleMain {
         return word;
     }
 
-    //returns a random word from possibleWords
-    public static String getWordle() throws IOException {
-        String fileContents = Files.readString(Paths.get("./usableWords/possibleWordles"));
-        String[] wordlesArray = fileContents.split(" ");
+    //ansi colors for setKeyboardLetterColor
+    private static final String GREEN = "\u001B[42m";
+    private static final String YELLOW = "\u001B[43m";
+    private static final String WHITE = "\u001B[47m";
+    private static final String RESET = "\u001B[0m";
 
-        Random random = new Random();
-        String wordle = wordlesArray[random.nextInt(wordlesArray.length)];
+    private static final char[][] keys = {"QWERTYUIOP".toCharArray(), "ASDFGHJKL".toCharArray(), "ZXCVBNM".toCharArray()};
+    private static String[][] keyColors = {{RESET, RESET, RESET, RESET, RESET, RESET, RESET, RESET, RESET, RESET},
+            {RESET, RESET, RESET, RESET, RESET, RESET, RESET, RESET, RESET},
+            {RESET, RESET, RESET, RESET, RESET, RESET, RESET}};
 
-        return wordle;
+    private static Character[] charArrayToCharObjArray(char[] chars) {
+        Character[] output = new Character[chars.length];
+
+        for (int character = 0; character < chars.length; character++) {
+            output[character] = Character.valueOf(chars[character]);
+        }
+
+        return output;
+    }
+
+    //sets the ansiColor of a particular key on the keyboard (not guessed, not in the word, not in the right place, in the right place)
+    public static void setKeyboardLetterColor(char letter, String ansiColor) {
+
+        for (int row = 0; row < keys.length; row++) {
+            if (Arrays.asList(charArrayToCharObjArray(keys[row])).contains(Character.valueOf(letter))) {
+                keyColors[row][new String(keys[row]).indexOf(letter)] = ansiColor;
+                return;
+            }
+        }
+    }
+
+    public static void printKeyboard() {
+        String keyboard = "";
+
+        for (int row = 0; row < keys.length; row++) {
+            for (int key = 0; key < keys[row].length; key++) {
+                keyboard += keyColors[row][key];
+                keyboard += keys[row][key];
+            }
+
+            keyboard += "\n";
+        }
+
+        System.out.println(keyboard);
     }
 }
